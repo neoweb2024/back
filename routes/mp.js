52a710d77f60727090b9607c2e9ea735b65e0cc8
alejoguzmann <alejoguzmann20@gmail.com>
2024-03-18 17:46:34 -0300

    listo para crear, todo comentado

--- routes/mp.js
+++ routes/mp.js
@@ -11,9 +11,15 @@ const client = new MercadoPagoConfig({
   accessToken: TOKEN,
 });
 
+const payment = new Payment(client);
+
 // POST
 router.post("/crear-preferencia", async (req, res) => {
   try {
+
+    const appo = {
+      nombre : "nicoa" //meter datos del turno
+    }
 
     const body = {
       items: [
@@ -29,7 +35,8 @@ router.post("/crear-preferencia", async (req, res) => {
         failure: "https://proyecto-neo.vercel.app/reserva-error",
       },
       auto_return: "approved",
-      "external_reference": "RiseUP",
+      external_reference: appo,
+      notification_url: "https://proyecto-neo-back-production.up.railway.app/mercadopago/webhook"
     };
 
     const preference = new Preference(client);
@@ -43,6 +50,29 @@ router.post("/crear-preferencia", async (req, res) => {
     res.status(500).json({
       error: error.message,
     });
+  }
+});
+
+router.post("/webhook", async (req, res) => {
+
+  let paymentQ = req.query;
+  try {
+
+    if (paymentQ.type === "payment") {
+      const result = await payment.get({
+        id: paymentQ["data.id"],
+      });
+      // console.log(result.external_reference, "soy el turno");
+
+      if (result.status === "approved") {
+        const appointmentPaid = result.external_reference //turno pagado
+        //CREAR TURNO
+      }
+      return res.status(200);
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
   }
 });
 
